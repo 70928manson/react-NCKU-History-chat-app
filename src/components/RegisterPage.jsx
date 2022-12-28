1948c64556c4e0848d00288b08d0593c72845cb8
Manson <manson972@yahoo.com>
2022-12-28 20:56:35 +0800

    login and register page update

---
+++ src/components/RegisterPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from "../styles/form.module.scss";
+import Add from "../images/addAvatar.png";
+
+const RegisterPage = () => {
+    return (
+        <div className={styles.formContainer}>
+            <div className={styles.formWrapper}>
+                <span className={styles.logo}>Manson Chat</span>
+                <span className={styles.title}>Register</span>
+                <form>
+                    <input type="text" placeholder="display name"></input>
+                    <input type="email" placeholder="email"></input>
+                    <input type="password" placeholder="password"></input>
+                    <input style={{ display: 'none' }} type="file" id="file"/>
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button>Sign up</button>
+                </form>
+                <p>You do have an account? Login</p>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
